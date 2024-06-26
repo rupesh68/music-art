1e75333e022d921ade122835400227c28abe333e
Rupesh Mahajan <rupeshmahajan516@gmail.com>
2024-06-26 17:06:24 +0530

    minor fixes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -25,10 +25,11 @@ export default function Page() {
           programs, or events. Reach out and let us know how we can assist you
           in your musical journey.
         </p>
+        <form action="" onSubmit={handleSubmit} className="space-y-4 mt-4">
         <input
           type="email"
           value={email}
-            onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="hi@rupeshmahajan.in"
           className="rounded-lg border p-4 border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
         />
@@ -38,14 +39,20 @@ export default function Page() {
           placeholder="Your message"
           className="rounded-lg border p-4 border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
         ></textarea>
-      </div>
-      <button
+         <button
         type="submit"
         className="px-6 py-2 rounded-lg bg-teal-500 text-white font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
       >
         {" "}
         Send Message
       </button>
+
+
+        </form>
+      
+      </div>
+     
+      
       <BackgroundBeams />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,17 @@ import MovingCards from "@/components/MovingCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Instructors from "@/components/Instructors";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       <HeroSection />
-      <FeaturedCourses/>
-      <WhyChooseUs/>
-      <MovingCards/>
-      <UpcomingWebinars/>
-      <Instructors/>
-      <Footer/>
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MovingCards />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
